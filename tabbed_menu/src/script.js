df4ed0268f54ae2c346daca189c2f3a2905116e7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-18 08:00:25 -0400

    cleaned up

--- tabbed_menu/src/script.js
+++ tabbed_menu/src/script.js
@@ -1,7 +1,7 @@
 
 function openCity(cityName, elmnt, color) {
     var i, tabcontent, tablinks;
-    
+
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
