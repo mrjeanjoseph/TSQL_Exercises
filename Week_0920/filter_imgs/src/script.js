7d3d530686cd4e7040a8f845dc1d0b0299bad4b4
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-22 10:05:40 -0400

    proj completed

--- Week_0920/filter_imgs/src/script.js
+++ Week_0920/filter_imgs/src/script.js
@@ -1,4 +1,45 @@
 
 $(function() {
-    
+    var $filter = $('.filter');
+    var $tab = $('.filter a');
+    var $offers = $('.boxGroup .box');
+
+    $filter.on('click touch', '.all', function(e) {
+        e.preventDefault();
+        $tab.removeClass('current');
+        $(this).addClass('current');
+
+        $offers.hide();
+        $offers.fadeIn( 700 );
+    });
+
+    $filter.on('click touch', '.one', function(e) {
+        e.preventDefault();
+        $tab.removeClass('current');
+        $(this).addClass('current');
+
+        $offers.show();
+        $offers.fadeOut();
+        $offers.filter('.one').fadeIn( 400 );
+    });
+
+    $filter.on('click touch', '.two', function(e) {
+        e.preventDefault();
+        $tab.removeClass('current');
+        $(this).addClass('current');
+
+        $offers.show();
+        $offers.fadeOut();
+        $offers.filter('.two').fadeIn( 400 );
+    });
+
+    $filter.on('click touch', '.three', function(e) {
+        e.preventDefault();
+        $tab.removeClass('current');
+        $(this).addClass('current');
+
+        $offers.show();
+        $offers.fadeOut();
+        $offers.filter('.three').fadeIn( 400 );
+    });
 })
