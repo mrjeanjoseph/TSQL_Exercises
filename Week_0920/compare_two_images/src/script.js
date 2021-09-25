929f6c01642b0d1a676352e304a9b3acdfcec782
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-25 06:29:48 -0400

    daily projs commenced

---
+++ Week_0920/compare_two_images/src/script.js
@@ -0,0 +1,15 @@
+
+const person = [
+    {
+        fname: "Frednel",
+        lname: "Doe",
+        age: 25
+    }
+    
+]
+let txt = "";
+for (let x in person) {
+    txt += person[x] + " ";
+}
+
+document.getElementById("demo").innerHTML = txt;
