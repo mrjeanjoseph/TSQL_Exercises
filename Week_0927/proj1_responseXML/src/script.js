d284fbc6c4788f88237fa5bd480429cb12160457
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-09-28 09:37:23 -0400

    unable to get xml file to display

--- Week_0927/proj1_responseXML/src/script.js
+++ Week_0927/proj1_responseXML/src/script.js
@@ -1,3 +1,26 @@
 
-const pageTitle = 'Project Title goes here!';
+const pageTitle = 'XMLHttpRequst Object';
 document.getElementById("title").innerHTML = pageTitle;
+
+
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      myFunction(this);
+    }
+    xhttp.open("GET", "cd_catalog.xml");
+    xhttp.send();
+  }
+  function myFunction(xml) {
+    const xmlDoc = xml.responseXML;
+    const x = xmlDoc.getElementsByTagName("CD");
+    let table="<tr><th>Artist</th><th>Title</th></tr>";
+    for (let i = 0; i <x.length; i++) { 
+      table += "<tr><td>" +
+      x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
+  }
