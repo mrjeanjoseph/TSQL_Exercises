f3491e8b7a934bc9e102ed119489cd4d3de059d9
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-01 09:05:17 -0400

    proj js gen and it created

---
+++ Week_0927/proj1_responseXML/src/data.js
@@ -0,0 +1,30 @@
+Family = [
+    Husband = {
+        lastName: "Frednel",
+        firstName: "Jean-Joseph"
+    },
+    Wife = {
+        lastName: "Louna",
+        firstName: "Jean-Joseph"
+    },
+    GrandPa = {
+        lastName: "JnGuitaud",
+        firstName: "Paniague"
+    },
+    GrandMa = {
+        lastName: "Marie Denise",
+        firstName: "Paniague"
+    },
+    Son = {
+        lastName: "Devereaux",
+        firstName: "Jean-Joseph"
+    },
+    Daughter = {
+        lastName: "Veleenah",
+        firstName: "Jean-Joseph"
+    },
+    Son_Second = {
+        lastName: "Casteeleaux",
+        firstName: "Jean-Joseph"
+    }
+]
