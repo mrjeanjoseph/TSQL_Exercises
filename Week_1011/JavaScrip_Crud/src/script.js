84196a7b2e8d4699b152b419c9843495faa3b7e5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-16 20:45:31 -0400

    getting javascript crud started

---
+++ Week_1011/JavaScrip_Crud/src/script.js
@@ -0,0 +1,12 @@
+
+const pageTitle = 'Project Title goes here!';
+document.getElementById("title").innerHTML = pageTitle;
+
+
+var app = new function() {
+   this.el = document.getElementById('countries');
+   this.countries = ['Haiti', 'Chili', 'Equator', 'Nicaragua', 'Guatemala'];
+   this.Count = function(data) {
+       
+   }
+}
