bf31fd4d23b27c1a3db3425b50c4aaeb873860ea
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-16 23:18:45 -0400

    table build

--- Week_1011/JavaScrip_Crud/src/script.js
+++ Week_1011/JavaScrip_Crud/src/script.js
@@ -7,6 +7,10 @@ var app = new function() {
    this.el = document.getElementById('countries');
    this.countries = ['Haiti', 'Chili', 'Equator', 'Nicaragua', 'Guatemala'];
    this.Count = function(data) {
-       
+       var el = document.getElementById('counter');
+       var name = 'country';
+
+       if
+
    }
 }
