a1fd35f479fcdedf4ba877227274139a6593cc1b
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-23 08:26:31 -0400

    the add() method

--- Week_1018/weekend_practice/src/script.js
+++ Week_1018/weekend_practice/src/script.js
@@ -1,3 +1,7 @@
 
 const pageTitle = 'Project Title goes here!';
 document.getElementById("title").innerHTML = pageTitle;
+
+$(document).ready(function() {
+    $("li").css("background-color", "red");
+})
