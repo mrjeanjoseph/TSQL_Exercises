733bcc38010916116e426268d8d58b059ba94819
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-23 12:00:13 -0400

    short example

--- Week_1018/weekend_practice/src/script.js
+++ Week_1018/weekend_practice/src/script.js
@@ -2,6 +2,15 @@
 const pageTitle = 'Project Title goes here!';
 document.getElementById("title").innerHTML = pageTitle;
 
-$(document).ready(function() {
-    $("li").css("background-color", "red");
-})
+$("p").add("div").addClass("widget");
+var pdiv = $("p").add("div");
+
+// $("li").add("h1").add("p").css("background-color", "red");
+
+//$("li").add( document.getElementsByTagName("p")[0]).css("background-color","red");
+
+//$("li").add("<p id='new'> new paragraph </p>").css("background-color","red");
+
+$("div").css("border","2px solid red")
+        .add("p")
+        .css("background","yellow");
