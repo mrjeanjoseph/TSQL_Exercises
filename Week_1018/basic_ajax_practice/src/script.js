beb4985370a969a9c4b8307cb445cfb6cfaae062
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-10-24 15:27:12 -0400

    js reading dir files

--- Week_1018/basic_ajax_practice/src/script.js
+++ Week_1018/basic_ajax_practice/src/script.js
@@ -4,23 +4,26 @@
 (function () {
     var columnOne = document.getElementById('column1');  // change to match image ID
     var columnTwo = document.getElementById('column2');  // change to match image ID
-    var imgDir1 = 'Images/GalleryOne/img';               // change to match images folder
-    var imgDir2 = 'Images/GalleryOne/img';               // change to match images folder
+    var imgDir1 = 'Images/img';               // change to match images folder
+    var imgDir2 = 'Images/img';               // change to match images folder
     var delayInSeconds = 1;                             // set number of seconds delay    
 
     // Yes I made changes below this line
-    var num1 = 0, num2 = 0;
-     
+    var numRound = Math.floor(Math.random() * 10);
+    var num1 = 0, num2 = numRound;
+    
+    
     var changeImage = function () {
         columnOne.src = imgDir1 + num1++ + ".jpg";
         columnTwo.src = imgDir2 + num2++ + ".jpg";
 
-        if (num1 == 5) {
-            num1 = 0; num2 = 0;
+        if (num1 == 16) {
+            num1 = numRound;
+            num2 = 0;
             
         }
     };
-    setInterval(changeImage, delayInSeconds * 1000);
+   setInterval(changeImage, delayInSeconds * 1000);
 })();
 
 
