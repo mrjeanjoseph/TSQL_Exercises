db14583de5c2393f4b255ea16aacf7d7482b9b6f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-11-07 07:52:30 -0500

    retrieve and search mothods

--- Week_1101/AyisyenPie_API/index.js
+++ Week_1101/AyisyenPie_API/index.js
@@ -1,10 +1,21 @@
 let express = require('express');
 let app = express();
 
+//use express Router Object
 let router = express.Router();
 
+//Creating an array of items to be passed in
+let pies = [
+    {"id":1,"name":"Confiti Pie"},
+    {"id":2,"name":"Gwayav Pie"},
+    {"id":3,"name":"Kashiman Pie"},
+    {"id":4,"name":"Kowosol Pie"}
+];
+
+
 router.get('/', function(req, res, next) {
-    res.send('Ayisyen Pie Dous');
+    //Here I return the array of object
+    res.status(200).send(pies);
 });
 
 app.use('/api/', router);
