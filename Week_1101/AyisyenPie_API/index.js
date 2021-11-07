54ceac33b88550b113b9ef60f797333d21f52917
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-11-07 09:53:57 -0500

    retrieve and search method complete

--- Week_1101/AyisyenPie_API/index.js
+++ Week_1101/AyisyenPie_API/index.js
@@ -1,25 +1,30 @@
 let express = require('express');
 let app = express();
+let pieRepo = require('./repos/pieRepo');
 
 //use express Router Object
 let router = express.Router();
-
 //Creating an array of items to be passed in
-let pies = [
-    {"id":1,"name":"Confiti Pie"},
-    {"id":2,"name":"Gwayav Pie"},
-    {"id":3,"name":"Kashiman Pie"},
-    {"id":4,"name":"Kowosol Pie"}
-];
+//let pies = pieRepo.get();
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     //Here I return the array of object
-    res.status(200).send(pies);
+    pieRepo.get(function (data) {
+        res.status(200).json({
+            //wrapping the status message inside a json envelop
+            "status": 200,
+            "statusText": "OK",
+            "message": "Tout Gato yo valab.",
+            "data": data
+        });
+    }, function (err) {
+        next(err);
+    });
 });
 
 app.use('/api/', router);
 
-var server = app.listen(5000, function() {
+var server = app.listen(5000, function () {
     console.log('Node server is running on http://localhost:5000...,');
 });
