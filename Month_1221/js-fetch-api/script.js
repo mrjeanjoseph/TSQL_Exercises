73cf683e612da16e3b5052f521fbb9be26ac7148
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-04 13:34:41 -0500

    issue resolved. getting data from github dog api

--- Month_1221/js-fetch-api/script.js
+++ Month_1221/js-fetch-api/script.js
@@ -9,12 +9,28 @@
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await response.json();
-    console.log(data.message);
+    //console.log(data.message);
     createBreedList(data.message);
 }
 
 start();
 
-function createBreedList() {
-    document.getElementById("breed").innerHTML = 'hey';
+function createBreedList(breedList) {
+    document.getElementById("breed").innerHTML = `
+    <select onchange="loadByBreed(this.value)">
+        <option>Choose a dog Breed</option>
+        ${Object.keys(breedList).map(function(breed){
+            return `<option>${breed}</option>`
+        }).join("")}
+    </select>
+    `;
+}
+
+async function loadByBreed(breed){
+    if(breed != "Choose a dog Breed"){
+        //alert("You chose a " + breed);
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+        const data = await response.json();
+        console.log(data)
+    }
 }
