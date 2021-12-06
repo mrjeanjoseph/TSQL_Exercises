3ff8232cf16ba3a55765eb068d08b38b47d7484f
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-05 20:21:11 -0500

    reconstruct the entire repo

--- excel-to-javascript/load-excel-to-javascript/script.js
+++ excel-to-javascript/load-excel-to-javascript/script.js
@@ -14,13 +14,13 @@ excel_file.addEventListener("change", (event) => {
             var table_output = `<table class="table table-striped table-bordered">`;
             for (var row = 0; row < sheet_data.length; row++) {
                 table_output += '<tr>'
-                for (var cell = 0; cell < sheet_data[row].length; cell) {
+                for (var cell = 0; cell < sheet_data[row].length; cell++) {
                     table_output += `<td>${sheet_data[row][cell]}</td>`
                 }
-
                 table_output += '</tr>'
             }
             table_output += '</table>'
+            document.getElementById('excel_data').innerHTML = table_output;
         }
     }
 });
