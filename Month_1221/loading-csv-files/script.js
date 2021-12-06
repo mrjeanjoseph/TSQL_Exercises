7d5a0dffaae76b6b270b487579d110516d0b857c
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-05 19:44:58 -0500

    excel-to-HTML proj

--- Month_1221/loading-csv-files/script.js
+++ Month_1221/loading-csv-files/script.js
@@ -4,25 +4,27 @@ $(document).ready(function () {
             url: "employee.csv",
             dataType: "text",
             success: function (data) {
-                let employee_data = data.split(/\r?\n|r/);
-                let table_data = `
-                <table class="table table-bordered table-striped">
-                `;
-                for (let count = 0; count < employee_data.length; count++) {
-                    let cell_data = employee_data[count].split(",");
-                    cell_data += 'tr';
-                    for (let cell_count = 0; cell_count < cell_data.length; cell_count++) {
-                        if (count === 0) {
-                            table_data += `<th>${cell_data[cell_data.length]}</th>`;
-                        } else {
-                            table_data += `<td>${cell_data[cell_data.length]}</td>`;
-                        }
-                    }
-                    table_data += '</tr>';
-                }
-                table_data +='</table>';
-                $('#employee_table').html(table_data);
+                loadTableData(data);
             }
-        })
-    })
-})
+        });
+    });
+});
+
+function loadTableData(data){
+    var employee_data = data.split(/\r?\n|\r/);
+    var table_data = `<table class="table table-bordered table-striped">`;               
+    for (var count = 0; count < employee_data.length; count++) {
+        var cell_data = employee_data[count].split(",");
+        table_data += '<tr>';
+        for (var cell_count = 0; cell_count < cell_data.length; cell_count++) {
+            if (count === 0) {
+                table_data += `<th>${cell_data[cell_count]}</th>`;
+            } else {
+                table_data += `<td>${cell_data[cell_count]}</td>`;
+            }
+        }
+        table_data += '</tr>';
+    }
+    table_data +='</table>';
+    $('#employee_table').html(table_data);
+}
