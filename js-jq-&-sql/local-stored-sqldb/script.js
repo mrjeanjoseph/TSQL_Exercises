d34032f65c7a86b2a52e67d77201d19f6193fe96
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-07 09:32:09 -0500

    crud process successfully

--- js-jq-&-sql/local-stored-sqldb/script.js
+++ js-jq-&-sql/local-stored-sqldb/script.js
@@ -1,29 +1,89 @@
 
 var db = openDatabase("itemDB", "1.0", "itemDB", 65535);
 
-$(document).ready(function () {
-
-    $("#create").click(function () {
+function createTable(){
+        $("#create").click(function () {
         db.transaction(function (transaction) {
             var sql = "CREATE TABLE items " +
                 "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
                 "item VARCHAR(100) NOT NULL," +
                 "quantity INT(5) NOT NULL)";
             transaction.executeSql(sql, undefined, function () {
-                alert("Table is created Successfully");
+                alert("Table created Successfully");
             }, function () {
-                alert("Data is created Successfully");
+                alert("An error occurred while create table");
             })
-        })
-    })
+        });
+    });
+}
 
-    $("#remove").click(function () {
+function deleteTableData(){
+        $("#remove").click(function () {
         if (!confirm("Are you sure to delete this table?", "")) return;;
         db.transaction(function (transaction) {
-            var sql = "DELETE TABLE items";
+            var sql = "DROP TABLE items";
             transaction.executeSql(sql, undefined, function () {
-
+                alert("Table deleted successfully.");
+            }, function (transaction, error) {
+                alert("An error occurred while deleting table\n" + error.message);
             })
-        })
-    })
-})
+        });
+    });
+}
+
+function insertDataIntoTable(){
+        $("#insert").click(function () {
+        var item = $("#item").val();
+        var qty = $("#quantity").val();
+        db.transaction(function (transaction) {
+            var sql = "INSERT INTO items(item,quantity)" +
+                "VALUES(?,?)";
+            transaction.executeSql(sql, [item, qty], function () {
+                alert(`Items: ${item} added successfully`);
+            }, function (transaction, error) {
+                alert("Error adding items\n" + error.message);
+            });
+        });
+    });
+}
+
+$(document).ready(function () {
+
+createTable();
+deleteTableData();
+insertDataIntoTable();
+
+    $("#list").click(function () {
+        $("#table-data").children().remove();
+        db.transaction(function (transaction) {
+            var sql = "SELECT * FROM items ORDER BY id DESC";
+            transaction.executeSql(sql, undefined, function (transaction, result) {
+                if (result.rows.length) {
+                    for (let data = 0; data < result.rows.length; data++) {
+                        let row = result.rows.item(data);
+                        let item = row.item;
+                        let id = row.id;
+                        let qty = row.quantity;
+                        $("#table-data").append(`
+                            <tr>
+                                <td>${id}</td>
+                                <td>${item}</td>
+                                <td>${qty}</td>
+                            </tr>
+                        `);
+                    }
+
+                } else {
+                    $("#table-data").append(`
+                        <tr>
+                            <td colspan="3" align="center">Fetched records returns no data</td>
+                        </tr>
+                    `);
+                }
+            }, function (transaction, error) {
+                alert("An error getting data", error.message);
+            });
+        });
+    });
+
+});
