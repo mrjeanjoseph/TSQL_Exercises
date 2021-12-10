c31ea339ffe9d5fd163639880a71d9d8ad4dc1f5
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-09 20:23:28 -0500

    post completed

---
+++ advanced-node/multiple-fileupload/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const fileUpload = require('express-fileUpload');
+const { join } = require('path');
+const path = require('path');
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'tmp'),
+    createParentPath: true,
+    limits: {fileSize: 1024}
+}));
+
+app.get('/', async (req, res, next) => {
+    res.render('index');
+});
+
+function randomStr(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));            
+    }
+    return result;
+}
+
+//console.log(randomStr(5));
+
+app.post('/single', async (req, res, next) => {
+    try {
+        const file = req.files.mFile;
+        console.log(file);
+
+        //const fileName = new Date().getTime().toString() + path.extname(file.name);
+        const fileName = randomStr(5) + path.extname(file.name);
+
+        const savePath = path.join(__dirname, 'public', 'uploads', fileName)
+        await file.mv(savePath);
+        //res.redirect('/')
+        res.send('FILE UPLOADED');
+
+    } catch (error) {
+        console.log(error);
+        //res.redirect('/errorpage');
+        res.send('Error uploading file');
+    }
+})
+
+var port = 1700;
+app.listen(port, () => console.log(`listening on ${port}.`));
