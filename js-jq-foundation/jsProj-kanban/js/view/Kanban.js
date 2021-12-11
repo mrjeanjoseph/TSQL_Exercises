423a8427199dfb0387029278e61d275d393fd867
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-11 09:06:57 -0500

    adding the column view section

--- js-jq-foundation/jsProj-kanban/js/view/Kanban.js
+++ js-jq-foundation/jsProj-kanban/js/view/Kanban.js
@@ -1,1 +1,23 @@
-export default 
+export default class Kanban {
+    constructor(root) {
+        this.root = root;
+
+        Kanban.columns().forEach(column => {
+           //TO DO: Create an instance of column class 
+        });
+    }
+
+    static columns() {
+        return [
+            {
+                id: 1, title: "Not Started"
+            },
+            {
+                id: 2, title: "In Progress"
+            },
+            {
+                id: 3, title: "Completed"
+            }
+        ]
+    }
+}
