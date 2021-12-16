f65f00a4bfac024ff3e83a219a43d9baf7390138
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-16 07:03:02 -0500

    gotten extra dependencies

--- advanced-node/server-side-validation/index.js
+++ advanced-node/server-side-validation/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 999;
 const path = require("path");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
-let urlencoded = bodyParser.urlencoded({extended: false});
+let urlencoded = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.json());
 app.use(urlencoded);
@@ -14,8 +15,20 @@ app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post('/formData', (request, response) => {
-    console.log(request.body.name);
-});
+app.post("/formData", [
+    check('name')
+    .not().isEmpty()
+], (request, response) => {
+    const errors = validationResult(request);
+
+    if(!errors.isEmpty()){
+        return response.status(422).json({
+            errors: errors.array()
+        });
+    }
+    response.status(202).json({
+        success: 'Ok!'
+    })
+})
 
 app.listen(port, () => console.log("server listening on port 999"))
