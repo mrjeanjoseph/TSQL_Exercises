7dde4a140d80b213f2230a845f3011f2c5c763fc
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-12-24 11:06:22 -0500

    still wirig up the dom

--- js-jq-foundation/local-storage-note/src/script.js
+++ js-jq-foundation/local-storage-note/src/script.js
@@ -2,17 +2,28 @@ let addBtn = document.getElementById("add-btn");
 let addTitle = document.getElementById("note-title");
 let addTxt = document.getElementById("note-text");
 
-addBtn.addEventListener("click", (e) => {
-    if(addTitle.value == "" || addTxt.value == "") {
-        return alert("Please add your notes");
-    }
-
-    let notes = localStorage.getItem("notes");
+function localStorageNotes(notes){
+    notes = localStorage.getItem("notes");
     if(notes == null){
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
+    return notes;
+}
+addBtn.addEventListener("click", (e) => {
+    if(addTitle.value == "" || addTxt.value == "") {
+        return alert("Please add your notes");
+    }
+
+    // let notes = localStorage.getItem("notes");
+    // if(notes == null){
+    //     notesObj = [];
+    // } else {
+    //     notesObj = JSON.parse(notes);
+    // }
+    // let notes
+    // localStorageNotes(notes);
     let myObj = {
         title: addTitle.value,
         text: addTxt.value
@@ -23,29 +34,57 @@ addBtn.addEventListener("click", (e) => {
     addTitle.value = "";
     addTxt.value = "";
 
-    showNotes();
+    // showNotes();
 })
+showNotes();
 
 // Show notes on the page
-
 function showNotes(){
-    let notes = localStorage.getItem("notes");
-    if(notes == null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    }
+    // let notes = localStorage.getItem("notes");
+    // if(notes == null) {
+    //     notesObj = [];
+    // } else {
+    //     notesObj = JSON.parse(notes);
+    // }
+    let notes
+    localStorageNotes(notes);
 
     let html = "";
     notesObj.forEach(function(element, index) {
         html += `
             <div id="note">
-                <p class="note-counter">Note 1</p>
-                <h3 class="note-title">Hello World</h3>
-                <p class="note-text">Some Note text</p>
-                <button class="note-btn">Delete Note</button>
-                <button class="note-btn edit-btn">Edit Note</button>
+                <p class="note-counter">Note ${ index + 1 }</p>
+                <h3 class="note-title">${ element.title }</h3>
+                <p class="note-text">${ element.text }</p>
+                <button id="${index}" onclick="deleteNote(this.id)" class="note-btn">Delete Note</button>
+                <button id="${index}" onclick="editNote(this.id)" class="note-btn edit-btn">Edit Note</button>
             </div>
-        `
-    })
+        `;
+    });
+
+    let noteElem = document.getElementById("notes");
+    if(notesObj.length !=0) {
+        noteElem.innerHTML = html;
+    } else {
+        noteElem.innerHTML = "No Notes Found! \n Add Notes using the form above";
+    }
+}
+
+function deleteNote(index) {
+    let confirmDelete = confirm("You are about to delete this note forever !!!");
+    
+    if(confirmDelete == true) {
+        // let notes = localStorage.getItem("notes");
+        // if(notes == null) {
+        //     notesObj = [];
+        // } else {
+        //     notesObj = JSON.parse(notes);
+        // }
+        let notes
+        localStorageNotes(notes);
+
+        notesObj.splice(index, 1);
+        localStorage.setItem("notes", JSON.stringify(notesObj));
+        showNotes();
+    }
 }
