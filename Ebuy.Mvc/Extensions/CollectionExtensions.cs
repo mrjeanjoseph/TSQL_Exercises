9fb7389a1f2d0c4eaab75e429bbfd0d0d84c46cc
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-01-07 22:58:38 -0500

    eBuy-13, logging error persist

--- Ebuy.Mvc/Extensions/CollectionExtensions.cs
+++ Ebuy.Mvc/Extensions/CollectionExtensions.cs
@@ -2,12 +2,9 @@
 using System.Diagnostics.Contracts;
 using System.Linq;
 
-namespace Ebuy
-{
-    public static class CollectionExtensions
-    {
-        public static IEnumerable<T> Page<T>(this IEnumerable<T> source, int pageIndex, int pageSize)
-        {
+namespace Ebuy {
+    public static class CollectionExtensions {
+        public static IEnumerable<T> Page<T>(this IEnumerable<T> source, int pageIndex, int pageSize) {
             Contract.Requires(pageIndex >= 0, "Page index cannot be negative");
             Contract.Requires(pageSize >= 0, "Page size cannot be negative");
 
@@ -21,8 +18,7 @@ namespace Ebuy
             return source;
         }
 
-        public static IQueryable<T> Page<T>(this IQueryable<T> source, int pageIndex, int pageSize)
-        {
+        public static IQueryable<T> Page<T>(this IQueryable<T> source, int pageIndex, int pageSize) {
             Contract.Requires(pageIndex >= 0, "Page index cannot be negative");
             Contract.Requires(pageSize >= 0, "Page size cannot be negative");
 
