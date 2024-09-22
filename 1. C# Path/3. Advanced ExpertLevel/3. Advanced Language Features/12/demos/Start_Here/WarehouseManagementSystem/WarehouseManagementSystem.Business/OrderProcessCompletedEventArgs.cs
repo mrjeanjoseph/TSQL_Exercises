d4a5b8ec06242de5b9858313e22bf241224eb110
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-09-22 14:35:36 -0400

    renamed all files again

--- 1. C# Path/3. Advanced ExpertLevel/3. Advanced Language Features/12/demos/Start_Here/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessCompletedEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using WarehouseManagementSystem.Domain;
-
-namespace WarehouseManagementSystem.Business
-{
-    public class OrderProcessCompletedEventArgs
-    {
-        public Order? Order { get; set; }
-    }
-}
