4c828babc80f43fc89582b4580c5eca2fa506d79
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-10-01 05:54:40 -0400

    new course - common pitfalls

--- 1. C# Path/3. Advanced ExpertLevel/4. Dependency Injection/05/demos/5.7 WithIDisposable/ProductImporter.Logic/Source/CsvProductSourceOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductImporter.Logic.Source
-{
-    public class CsvProductSourceOptions
-    {
-        public const string SectionName = "CsvProductSource";
-
-        public string SourceCsvPath { get; set; }
-    }
-}
