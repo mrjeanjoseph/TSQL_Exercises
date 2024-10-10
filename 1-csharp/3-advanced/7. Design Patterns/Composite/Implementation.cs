3ec095d4558aedaf96c30ac1f82a9a5adf2033f0
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-10-10 19:26:13 -0400

    covered two courses

--- 1-csharp/3-advanced/7. Design Patterns/Composite/Implementation.cs
+++ 1-csharp/3-advanced/7. Design Patterns/Composite/Implementation.cs
@@ -1,70 +1,67 @@
-﻿namespace Composite
+﻿namespace Composite;
+
+/// <summary>
+/// Component
+/// </summary>
+public abstract class FileSystemItem
 {
-    /// <summary>
-    /// Component
-    /// </summary>
-    public abstract class FileSystemItem
+    public string Name { get; set; }
+
+    public abstract long GetSize();
+
+    public FileSystemItem(string name)
     {
-        public string Name { get; set; }
+        Name = name;
+    }
+}
 
-        public abstract long GetSize();
-
-        public FileSystemItem(string name)
-        {
-            Name = name;
-        }
+/// <summary>
+/// Leaf
+/// </summary>
+public class File : FileSystemItem
+{
+    private long _size;
+    public File(string name, long size) : base(name)
+    {
+        _size = size;
     }
 
-    /// <summary>
-    /// Leaf
-    /// </summary>
-    public class File : FileSystemItem
+    public override long GetSize()
     {
-        private long _size;
-        public File(string name, long size) : base(name)
-        {
-            _size = size;
-        }
+        return _size;
+    }
+}
 
-        public override long GetSize()
-        {
-            return _size;
-        }
+/// <summary>
+/// Composite
+/// </summary>
+public class Directory : FileSystemItem
+{
+    private List<FileSystemItem> _fileSystemItems = new List<FileSystemItem>();
+
+    private long _size;
+    public Directory(string name, long size) : base(name)
+    {
+        _size = size;
     }
 
-    /// <summary>
-    /// Composite
-    /// </summary>
-    public class Directory : FileSystemItem
+    public void Add(FileSystemItem itemToAdd)
     {
-        private List<FileSystemItem> _fileSystemItems = new List<FileSystemItem>();
-
-        private long _size;
-        public Directory(string name, long size) : base(name)
-        {
-            _size = size;
-        }
-
-        public void Add(FileSystemItem itemToAdd)
-        {
-            _fileSystemItems.Add(itemToAdd);
-        }
-
-        public void Remove(FileSystemItem itemToRemove)
-        {
-            _fileSystemItems.Remove(itemToRemove);
-        }
-
-        public override long GetSize()
-        {
-            var treeSize = _size;
-            foreach (var fileSystemItem in _fileSystemItems)
-            {
-                treeSize += fileSystemItem.GetSize();
-            }
-            return treeSize;
-        }
+        _fileSystemItems.Add(itemToAdd);
     }
 
+    public void Remove(FileSystemItem itemToRemove)
+    {
+        _fileSystemItems.Remove(itemToRemove);
+    }
 
+    public override long GetSize()
+    {
+        var treeSize = _size;
+        foreach (var fileSystemItem in _fileSystemItems)
+        {
+            treeSize += fileSystemItem.GetSize();
+        }
+        return treeSize;
+    }
 }
