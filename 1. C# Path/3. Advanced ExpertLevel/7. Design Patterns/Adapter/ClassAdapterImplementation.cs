889a9bf4d204b65b06088d081719582c665380d7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-10-10 05:40:45 -0400

    couse dp bridge

--- 1. C# Path/3. Advanced ExpertLevel/7. Design Patterns/Adapter/ClassAdapterImplementation.cs
+++ 1. C# Path/3. Advanced ExpertLevel/7. Design Patterns/Adapter/ClassAdapterImplementation.cs
@@ -1,64 +1,63 @@
-﻿namespace ClassAdapter
+﻿namespace ClassAdapter;
+
+public class CityFromExternalSystem
 {
-    public class CityFromExternalSystem
-    {
-        public string Name { get; private set; }
-        public string NickName { get; private set; }
-        public int Inhabitants { get; private set; }
+    public string Name { get; private set; }
+    public string NickName { get; private set; }
+    public int Inhabitants { get; private set; }
 
-        public CityFromExternalSystem(string name, string nickName, int inhabitants)
-        {
-            Name = name;
-            NickName = nickName;
-            Inhabitants = inhabitants;
-        }
+    public CityFromExternalSystem(string name, string nickName, int inhabitants)
+    {
+        Name = name;
+        NickName = nickName;
+        Inhabitants = inhabitants;
     }
+}
 
-    /// <summary>
-    /// Adaptee
-    /// </summary>
-    public class ExternalSystem
+/// <summary>
+/// Adaptee
+/// </summary>
+public class ExternalSystem
+{
+    public CityFromExternalSystem GetCity()
     {
-        public CityFromExternalSystem GetCity()
-        {
-            return new CityFromExternalSystem("Antwerp", "'t Stad", 500000);
-        }
+        return new CityFromExternalSystem("Antwerp", "'t Stad", 500000);
     }
+}
 
-    public class City
+public class City
+{
+    public string FullName { get; private set; }
+    public long Inhabitants { get; private set; }
+
+    public City(string fullName, long inhabitants)
     {
-        public string FullName { get; private set; }
-        public long Inhabitants { get; private set; }
-
-        public City(string fullName, long inhabitants)
-        {
-            FullName = fullName;
-            Inhabitants = inhabitants;
-        }
+        FullName = fullName;
+        Inhabitants = inhabitants;
     }
+}
 
-    /// <summary>
-    /// Target
-    /// </summary>
-    public interface ICityAdapter
+/// <summary>
+/// Target
+/// </summary>
+public interface ICityAdapter
+{
+    City GetCity();
+}
+
+/// <summary>
+/// Adapter
+/// </summary>
+public class CityAdapter : ExternalSystem, ICityAdapter
+{
+    public City GetCity()
     {
-        City GetCity();
-    }
+        // call into the external system 
+        var cityFromExternalSystem = base.GetCity();
 
-    /// <summary>
-    /// Adapter
-    /// </summary>
-    public class CityAdapter : ExternalSystem, ICityAdapter
-    {
-        public City GetCity()
-        {
-            // call into the external system 
-            var cityFromExternalSystem = base.GetCity();
-
-            // adapt the CityFromExternalCity to a City 
-            return new City(
-                $"{cityFromExternalSystem.Name} - {cityFromExternalSystem.NickName}"
-                , cityFromExternalSystem.Inhabitants);
-        }
+        // adapt the CityFromExternalCity to a City 
+        return new City(
+            $"{cityFromExternalSystem.Name} - {cityFromExternalSystem.NickName}"
+            , cityFromExternalSystem.Inhabitants);
     }
 }
--- 1. C# Path/3. Advanced ExpertLevel/7. Design Patterns/Bridge/Implementation.cs
+++ 1. C# Path/3. Advanced ExpertLevel/7. Design Patterns/Bridge/Implementation.cs
@@ -1,76 +1,74 @@
-﻿namespace Bridge
+﻿namespace Bridge;
+
+/// <summary>
+/// Abstraction
+/// </summary>
+public abstract class Menu
 {
-    /// <summary>
-    /// Abstraction
-    /// </summary>
-    public abstract class Menu
+    public readonly ICoupon _coupon = null!;
+    public abstract int CalculatePrice();
+    public Menu(ICoupon coupon)
     {
-        public readonly ICoupon _coupon = null!;
-        public abstract int CalculatePrice();
-        public Menu(ICoupon coupon)
-        {
-            _coupon = coupon;
-        }
+        _coupon = coupon;
     }
+}
 
-    /// <summary>
-    /// RefinedAbstraction
-    /// </summary>
-    public class VegetarianMenu : Menu
+/// <summary>
+/// RefinedAbstraction
+/// </summary>
+public class VegetarianMenu : Menu
+{
+    public VegetarianMenu(ICoupon coupon) : base(coupon)
     {
-        public VegetarianMenu(ICoupon coupon) : base(coupon)
-        {
-        }
-        public override int CalculatePrice()
-        {
-            return 20 - _coupon.CouponValue;
-        }
     }
-     
-    /// <summary>
-    /// RefinedAbstraction
-    /// </summary>
-    public class MeatBasedMenu : Menu
+    public override int CalculatePrice()
     {
-        public MeatBasedMenu(ICoupon coupon) : base(coupon)
-        {
-        }
-        public override int CalculatePrice()
-        {
-            return 30 - _coupon.CouponValue;
-        }
+        return 20 - _coupon.CouponValue;
     }
+}
+ 
+/// <summary>
+/// RefinedAbstraction
+/// </summary>
+public class MeatBasedMenu : Menu
+{
+    public MeatBasedMenu(ICoupon coupon) : base(coupon)
+    {
+    }
+    public override int CalculatePrice()
+    {
+        return 30 - _coupon.CouponValue;
+    }
+}
 
-    /// <summary>
-    /// Implementor
-    /// </summary>
-    public interface ICoupon
-    {
-        int CouponValue { get; }
-    }
+/// <summary>
+/// Implementor
+/// </summary>
+public interface ICoupon
+{
+    int CouponValue { get; }
+}
 
-    /// <summary>
-    /// ConcreteImplementor
-    /// </summary>
-    public class NoCoupon : ICoupon
-    {
-        public int CouponValue { get => 0; }
-    }
+/// <summary>
+/// ConcreteImplementor
+/// </summary>
+public class NoCoupon : ICoupon
+{
+    public int CouponValue { get => 0; }
+}
 
-    /// <summary>
-    /// ConcreteImplementor
-    /// </summary>
-    public class OneEuroCoupon : ICoupon
-    {
-        public int CouponValue { get => 1; }
-    }
+/// <summary>
+/// ConcreteImplementor
+/// </summary>
+public class OneEuroCoupon : ICoupon
+{
+    public int CouponValue { get => 1; }
+}
 
-    /// <summary>
-    /// ConcreteImplementor
-    /// </summary>
-    public class TwoEuroCoupon : ICoupon
-    {
-        public int CouponValue { get => 2; }
-    }
-
+/// <summary>
+/// ConcreteImplementor
+/// </summary>
+public class TwoEuroCoupon : ICoupon
+{
+    public int CouponValue { get => 2; }
 }
