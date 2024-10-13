4c5ca4612301b7afc1ddeb8d8210de0bd42de1d7
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2024-10-13 08:49:52 -0400

    shorten all the namespaces

--- 1-csharp/3-advanced/7. Design Patterns/Builder/Implementation.cs
+++ 1-csharp/3-advanced/7. Design Patterns/Builder/Implementation.cs
@@ -1,119 +1,118 @@
 ﻿using System.Text;
 
-namespace BuilderPattern
+namespace BuilderPattern;
+
+/// <summary>
+/// Product
+/// </summary>
+public class Car
 {
-    /// <summary>
-    /// Product
-    /// </summary>
-    public class Car
+    private readonly List<string> _parts = new();
+    private readonly string _carType;
+
+    public Car(string carType)
     {
-        private readonly List<string> _parts = new();
-        private readonly string _carType;
-
-        public Car(string carType)
-        {
-            _carType = carType;
-        }
-
-        public void AddPart(string part)
-        {
-            _parts.Add(part);
-        }
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            foreach (string part in _parts)
-            {
-                sb.Append($"Car of type {_carType} has part {part}. ");
-            }
-
-            return sb.ToString();
-        }
+        _carType = carType;
     }
 
-    /// <summary>
-    /// Builder  
-    /// </summary>
-    public abstract class CarBuilder
+    public void AddPart(string part)
     {
-        public Car Car { get; private set; }
-
-        public CarBuilder(string carType)
-        {
-            Car = new Car(carType);
-        }
-
-        public abstract void BuildEngine();
-        public abstract void BuildFrame();
+        _parts.Add(part);
     }
 
-    /// <summary>
-    /// ConcreteBuilder1 class
-    /// </summary>
-    public class MiniBuilder : CarBuilder
+    public override string ToString()
     {
-        public MiniBuilder()
-            : base("Mini")
+        var sb = new StringBuilder();
+        foreach (string part in _parts)
         {
+            sb.Append($"Car of type {_carType} has part {part}. ");
         }
 
-        public override void BuildEngine()
-        {
-            Car.AddPart("'not a V8'");
-        }
+        return sb.ToString();
+    }
+}
 
-        public override void BuildFrame()
-        {
-            Car.AddPart("'3-door with stripes'");
-        }
+/// <summary>
+/// Builder  
+/// </summary>
+public abstract class CarBuilder
+{
+    public Car Car { get; private set; }
+
+    public CarBuilder(string carType)
+    {
+        Car = new Car(carType);
     }
 
-    /// <summary>
-    /// ConcreteBuilder2 class
-    /// </summary>
-    public class BMWBuilder : CarBuilder
+    public abstract void BuildEngine();
+    public abstract void BuildFrame();
+}
+
+/// <summary>
+/// ConcreteBuilder1 class
+/// </summary>
+public class MiniBuilder : CarBuilder
+{
+    public MiniBuilder()
+        : base("Mini")
     {
-        // Invoke base class constructor
-        public BMWBuilder()
-            : base("BMW")
-        {
-        }
-
-        public override void BuildEngine()
-        {
-            Car.AddPart("'a fancy V8 engine'");
-        }
-
-        public override void BuildFrame()
-        {
-            Car.AddPart("'5-door with metallic finish'");
-        }
     }
 
-    /// <summary>
-    /// Director
-    /// </summary>
-    public class Garage
+    public override void BuildEngine()
     {
-        private CarBuilder? _builder;
-
-        public Garage()
-        {
-        }
-
-        public void Construct(CarBuilder builder)
-        {
-            _builder = builder;
-
-            _builder.BuildEngine();
-            _builder.BuildFrame();
-        }
-
-        public void Show()
-        {
-            Console.WriteLine(_builder?.Car.ToString());
-        }
-
+        Car.AddPart("'not a V8'");
     }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'3-door with stripes'");
+    }
+}
+
+/// <summary>
+/// ConcreteBuilder2 class
+/// </summary>
+public class BMWBuilder : CarBuilder
+{
+    // Invoke base class constructor
+    public BMWBuilder()
+        : base("BMW")
+    {
+    }
+
+    public override void BuildEngine()
+    {
+        Car.AddPart("'a fancy V8 engine'");
+    }
+
+    public override void BuildFrame()
+    {
+        Car.AddPart("'5-door with metallic finish'");
+    }
+}
+
+/// <summary>
+/// Director
+/// </summary>
+public class Garage
+{
+    private CarBuilder? _builder;
+
+    public Garage()
+    {
+    }
+
+    public void Construct(CarBuilder builder)
+    {
+        _builder = builder;
+
+        _builder.BuildEngine();
+        _builder.BuildFrame();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(_builder?.Car.ToString());
+    }
+
 }
--- 1-csharp/3-advanced/7. Design Patterns/Iterator/Implementation.cs
+++ 1-csharp/3-advanced/7. Design Patterns/Iterator/Implementation.cs
@@ -1,95 +1,94 @@
-﻿namespace Iterator
+﻿namespace Iterator;
+
+public class Person
 {
-    public class Person
-    {
-        public string Name { get; set; }
-        public string Country { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
 
-        public Person(string name, string country)
+    public Person(string name, string country)
+    {
+        Name = name;
+        Country = country;
+    }
+}
+
+/// <summary>
+/// Iterator
+/// </summary>
+public interface IPeopleIterator
+{
+    Person First();
+    Person Next();
+    bool IsDone { get; }
+    Person CurrentItem { get; }
+}
+
+/// <summary>
+/// Aggregate
+/// </summary>
+public interface IPeopleCollection
+{
+    IPeopleIterator CreateIterator();
+}
+
+/// <summary>
+/// ConcreteAggregate
+/// </summary>
+public class PeopleCollection : List<Person>, IPeopleCollection
+{
+    public IPeopleIterator CreateIterator()
+    {
+        return new PeopleIterator(this);
+    }
+}
+
+/// <summary>
+/// ConcreteIterator
+/// </summary>
+public class PeopleIterator : IPeopleIterator
+{
+    private PeopleCollection _peopleCollection;
+    private int _current = 0;
+
+    public PeopleIterator(PeopleCollection collection)
+    {
+        _peopleCollection = collection;
+    }
+
+    public bool IsDone
+    {
+        get { return _current >= _peopleCollection.Count; }
+    }
+
+
+    public Person CurrentItem
+    {
+        get
         {
-            Name = name;
-            Country = country;
+            return _peopleCollection
+              .OrderBy(p => p.Name).ToList()[_current];
         }
     }
 
-    /// <summary>
-    /// Iterator
-    /// </summary>
-    public interface IPeopleIterator
+
+    public Person First()
     {
-        Person First();
-        Person Next();
-        bool IsDone { get; }
-        Person CurrentItem { get; }
+        _current = 0;
+        return _peopleCollection
+            .OrderBy(p => p.Name).ToList()[_current];
     }
 
-    /// <summary>
-    /// Aggregate
-    /// </summary>
-    public interface IPeopleCollection
+    public Person Next()
     {
-        IPeopleIterator CreateIterator();
-    }
-
-    /// <summary>
-    /// ConcreteAggregate
-    /// </summary>
-    public class PeopleCollection : List<Person>, IPeopleCollection
-    {
-        public IPeopleIterator CreateIterator()
+        _current++;
+        if (!IsDone)
         {
-            return new PeopleIterator(this);
-        }
-    }
-
-    /// <summary>
-    /// ConcreteIterator
-    /// </summary>
-    public class PeopleIterator : IPeopleIterator
-    {
-        private PeopleCollection _peopleCollection;
-        private int _current = 0;
-
-        public PeopleIterator(PeopleCollection collection)
-        {
-            _peopleCollection = collection;
-        }
-
-        public bool IsDone
-        {
-            get { return _current >= _peopleCollection.Count; }
-        }
-
-
-        public Person CurrentItem
-        {
-            get
-            {
-                return _peopleCollection
-                  .OrderBy(p => p.Name).ToList()[_current];
-            }
-        }
-
-
-        public Person First()
-        {
-            _current = 0;
             return _peopleCollection
                 .OrderBy(p => p.Name).ToList()[_current];
         }
-
-        public Person Next()
+        else
         {
-            _current++;
-            if (!IsDone)
-            {
-                return _peopleCollection
-                    .OrderBy(p => p.Name).ToList()[_current];
-            }
-            else
-            {
-                return null;
-            }
+            return null;
         }
     }
 }
